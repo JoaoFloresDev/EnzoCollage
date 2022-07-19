526604d2c2ce68923fd87a8b89addd385bf14f5a
João Flores <joaoflores.dev@gmail.com>
2022-07-19 20:51:23 -0300

    Initial Commit

---
+++ EnzoCollageUITests/EnzoCollageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EnzoCollageUITestsLaunchTests.swift
+//  EnzoCollageUITests
+//
+//  Created by Joao Victor Flores da Costa on 19/07/22.
+//
+
+import XCTest
+
+class EnzoCollageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
