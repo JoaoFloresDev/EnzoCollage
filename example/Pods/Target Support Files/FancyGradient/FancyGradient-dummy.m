c0c5d753c5edd8f868e072e9f233b9b915bf1cf9
João Flores <joaoflores.dev@gmail.com>
2022-07-19 20:56:05 -0300

    teste

--- example/Pods/Target Support Files/FancyGradient/FancyGradient-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FancyGradient : NSObject
-@end
-@implementation PodsDummy_FancyGradient
-@end
--- example/Pods/Target Support Files/Pods-BouncyLayout_Example/Pods-BouncyLayout_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BouncyLayout_Example : NSObject
-@end
-@implementation PodsDummy_Pods_BouncyLayout_Example
-@end
